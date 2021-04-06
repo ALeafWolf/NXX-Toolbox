a18097f841dac020121830471fe21cadfe8f5d66
aleafwolf <51769728+ALeafWolf@users.noreply.github.com>
2021-04-06 15:52:19 -0400

    filter for card-selection completed

--- src/app/card-selection/card-selection.component.ts
+++ src/app/card-selection/card-selection.component.ts
@@ -8,14 +8,35 @@ import { DataService } from '../data-service/data.service';
 })
 export class CardSelectionComponent implements OnInit {
 
-  cards;
+  allCards: any[];
+  cards: any[];
+  filterConditions = ["All", "All", "All"];
 
   constructor(private _data: DataService) { }
 
   ngOnInit(): void {
-    this._data.getCards().subscribe((data) =>{
+    this._data.getCards().subscribe((data: any[]) =>{
+      this.allCards = data
       this.cards = data
     })
+  }
+
+  filterCards(){
+    let listHolder = []
+    this.allCards.forEach(card => {
+      let condition = this.filterConditions[0]
+      if(condition == "All" || card.character == condition){
+        condition = this.filterConditions[1]
+        if(condition == "All" || card.rarity == condition){
+          condition = this.filterConditions[2]
+          if(condition == "All" || card.type == condition){
+            listHolder.push(card)
+          }
+        }
+      }
+    });
+    console.log(listHolder)
+    this.cards = listHolder;
   }
 
 }
